815639400cc6d290b5faf135453857c126ce7ac6
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-18 12:06:21 +0100

    Add x_ii and y

--- exam-exx/tsp-drill/main.cpp
+++ exam-exx/tsp-drill/main.cpp
@@ -22,33 +22,29 @@ void setupLP(CEnv env, Prob lp, Data data) {
 	// add x vars
 	for (int i = 0; i < static_cast<int>(data.n); i++) {
 		for (int j = 0; j < static_cast<int>(data.n); j++) {
-			if (j != i){
-				char xtype = 'C';
-				double lb = 0.0;
-				double ub = data.n;
-				snprintf(name, NAME_SIZE, "x_%c_%c", data.node_names[i], data.node_names[j]);
-				char* xname = (char*)(&name[0]);
-				cout << xname << endl;
-				/*Obj function has no 'x_ij' variables, so put zero as coefficient (fifth parameter)"*/
-				CHECKED_CPX_CALL(CPXnewcols, env, lp, 1, 0, &lb, &ub, &xtype, &xname);
-				/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
-			}
-		}
+			char xtype = 'C';
+			double lb = 0.0;
+			double ub = data.n;
+			snprintf(name, NAME_SIZE, "x_%c_%c", data.node_names[i], data.node_names[j]);
+			char* xname = (char*)(&name[0]);
+			cout << xname << endl;
+			/*Obj function has no 'x_ij' variables, so put zero as coefficient (fifth parameter)"*/
+			CHECKED_CPX_CALL(CPXnewcols, env, lp, 1, 0, &lb, &ub, &xtype, &xname);
+			/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
+		 }
 	}
 	// add y vars 
 	for (int i = 0; i < static_cast<int>(data.n); i++) {
 		for (int j = 0; j < static_cast<int>(data.n); j++) {
-			if (j != i){
-				char xtype = 'B';
-				double lb = 0.0;
-				double ub = 1.0;
-				snprintf(name, NAME_SIZE, "y_%c_%c", data.node_names[i], data.node_names[j]);
-				char* xname = (char*)(&name[0]);
-				cout << xname << endl;
-				CHECKED_CPX_CALL(CPXnewcols, env, lp, 1, &(data.time_costs[i*data.n+j]), &lb, &ub, &xtype, &xname);
-				/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
-			}
-		}
+			char xtype = 'B';
+			double lb = 0.0;
+			double ub = 1.0;
+			snprintf(name, NAME_SIZE, "y_%c_%c", data.node_names[i], data.node_names[j]);
+			char* xname = (char*)(&name[0]);
+			cout << xname << endl;
+			CHECKED_CPX_CALL(CPXnewcols, env, lp, 1, &(data.time_costs[i*data.n+j]), &lb, &ub, &xtype, &xname);
+			/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
+		 }
 	}
 
     ///////////////////////////////////////////////////////////
@@ -63,11 +59,11 @@ void setupLP(CEnv env, Prob lp, Data data) {
 	//NB: no external loop, it's just a single constraint
         //idx contains the variables in constraint's left side (data.n-1 variables of course)
 	{
-		std::vector<int> idx(static_cast<int>(data.n)-1);
-        	std::vector<double> coef(static_cast<int>(data.n)-1, 1.0);   //coefficients for the variables in the constraints (data.n-1 coefs for data.n-1 vars)
-		for (int j = 0; j < static_cast<int>(data.n)-1; j++)
+		std::vector<int> idx(static_cast<int>(data.n));
+        	std::vector<double> coef(static_cast<int>(data.n), 1.0);   //coefficients for the variables in the constraints (data.n-1 coefs for data.n-1 vars)
+		for (int j = 0; j < static_cast<int>(data.n); j++)
 		{
-			idx[j] = data.zero_index*(data.n-1) + j; // corresponds to variable x_0j
+			idx[j] = data.zero_index*(data.n) + j; // corresponds to variable x_0j
 		}        
 		char sense = 'E';
 		
@@ -78,19 +74,19 @@ void setupLP(CEnv env, Prob lp, Data data) {
 	
 
 	// add incoming/outgoing flow constraints (2nd set of constraints from the model)
-	for (int k = 0; k < static_cast<int>(data.n)-1; k++) {
-		std::vector<int> idx1(static_cast<int>(data.n)-1);   //variables in the 1st summation
-		std::vector<int> idx2(static_cast<int>(data.n)-1);   //variables in the 2nd summation
+	for (int k = 0; k < static_cast<int>(data.n); k++) {
+		std::vector<int> idx1(static_cast<int>(data.n));   //variables in the 1st summation
+		std::vector<int> idx2(static_cast<int>(data.n));   //variables in the 2nd summation
 				
-		std::vector<double> coef1(static_cast<int>(data.n)-1, 1.0);  //coefs for the variables in the 1st summation
-		std::vector<double> coef2(static_cast<int>(data.n)-1, -1.0);  //coefs for the variables in the 2nd summation
+		std::vector<double> coef1(static_cast<int>(data.n), 1.0);  //coefs for the variables in the 1st summation
+		std::vector<double> coef2(static_cast<int>(data.n), -1.0);  //coefs for the variables in the 2nd summation
 				
 		char sense = 'E';
 		for (int i = 0; i < static_cast<int>(data.n); i++){
-			idx1[i] = i*(data.n-1) + k; // corresponds to variable x_ik
+			idx1[i] = i*(data.n) + k; // corresponds to variable x_ik
 		}
 		for (int j = 0; j < static_cast<int>(data.n); j++){
-			idx2[j] = k*(data.n-1) + j; // corresponds to variable x_kj
+			idx2[j] = k*(data.n) + j; // corresponds to variable x_kj
 		}
 		idx1.insert(idx1.end(), idx2.begin(), idx2.end());  //concatenate the idx's
 		coef1.insert(coef1.end(), coef2.begin(), coef2.end());  //concatenate the coef's
@@ -102,11 +98,11 @@ void setupLP(CEnv env, Prob lp, Data data) {
 
 	// add third set of constraints (forall i, sum{i,j} y_ij = 1)
 	for (int i = 0 ; i < static_cast<int>(data.n); i++) {
-		std::vector<int> idx(static_cast<int>(data.n)-1);
-		std::vector<double> coef(static_cast<int>(data.n)-1, 1.0);
-		for (int j = 0; j < static_cast<int>(data.n)-1; j++)
+		std::vector<int> idx(static_cast<int>(data.n));
+		std::vector<double> coef(static_cast<int>(data.n), 1.0);
+		for (int j = 0; j < static_cast<int>(data.n); j++)
 		{
-		      idx[j] = (data.n*(data.n-1)) + i*(data.n-1) + j; //corresponds to variable y_ij: (data.n*(data.n-1)) is the offset needed to get the y_ij's
+		      idx[j] = (data.n*(data.n)) + i*(data.n) + j; //corresponds to variable y_ij: (data.n*(data.n-1)) is the offset needed to get the y_ij's
 		}        
 		char sense = 'E';
 		
@@ -118,11 +114,11 @@ void setupLP(CEnv env, Prob lp, Data data) {
 	// add fourth set of constraints (forall j, sum{i,j} y_ij = 1)
 	//Like the previous, but doesn't trigger SIGSEGV
 	for (int j = 0 ; j < static_cast<int>(data.n); j++) {
-		std::vector<int> idx(static_cast<int>(data.n)-1);
-		std::vector<double> coef(static_cast<int>(data.n)-1, 1.0);
-		for (int i = 0; i < static_cast<int>(data.n)-1; i++)
+		std::vector<int> idx(static_cast<int>(data.n));
+		std::vector<double> coef(static_cast<int>(data.n), 1.0);
+		for (int i = 0; i < static_cast<int>(data.n); i++)
 		{
-		      idx[i] = (data.n*(data.n-1)) + i*(data.n-1) + j; //corresponds to variable y_ij: (data.n*(data.n-1)) is the offset needed to get the y_ij's
+		      idx[i] = (data.n*(data.n)) + i*(data.n) + j; //corresponds to variable y_ij: (data.n*(data.n-1)) is the offset needed to get the y_ij's
 		}        
 		char sense = 'E';
 		
@@ -133,10 +129,10 @@ void setupLP(CEnv env, Prob lp, Data data) {
 	
 	// add fifth set of constraints (linking constraints) (x_ij - N y_ij <= 0 -- all variables on the left side!!!)
 	for (int i = 0 ; i < static_cast<int>(data.n); ++i ) {
-		for (int j = 0 ; j < static_cast<int>(data.n)-1; ++j) {
+		for (int j = 0 ; j < static_cast<int>(data.n); ++j) {
 			std::vector<int> idx(2);
-			idx[0] = i*(data.n-1) + j;;        // x var
-			idx[1] = (data.n*(data.n-1)) + i*(data.n-1) + j;   // y var  (data.n*(data.n-1)) is the offset needed to get the y_ij's
+			idx[0] = i*(data.n) + j;;        // x var
+			idx[1] = (data.n*(data.n)) + i*(data.n) + j;   // y var  (data.n*(data.n-1)) is the offset needed to get the y_ij's
 			std::vector<double> coef(2); 
 			coef[0] = 1.0;
 			coef[1] = -data.n;

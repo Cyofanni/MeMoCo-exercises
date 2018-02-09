2aecd9b028152ba90699f8c6ee1d31769ac35d20
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2018-02-09 21:41:26 +0100

    handy scripts

---
+++ exam-exx/GenAlgoForTSP/results/launch60.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIMPOP=1500
+NUMGEN=500
+
+function printout {
+	echo "***RESULTS" $1 "Simulated Annealing" >> $5
+	printf "\n\n" >> $5
+	COUNTER=0
+	while [  $COUNTER -lt 7 ]; do
+             ../main ../instance_generator/tsp60.dat $DIMPOP $NUMGEN 0.2 $2 $3 $4 >> $5
+	     printf "\n\n\n" >> $5
+             let COUNTER=COUNTER+1 
+        done
+}
+
+#WITHOUT Simulated Annealing
+#firstarg: "with" or "without"
+#second and third: simulated annealing parameters
+#fourth: taken from command line, it's the known optimum
+#fifth: output file
+echo "BEGIN OF FILE" > $2
+
+RUNCOUNTER=0
+while [  $RUNCOUNTER -lt 7 ]; do
+    echo "RESULTS FROM RUN $RUNCOUNTER" >> $2
+    printf "\n" >> $2
+    printout "without" 0 0 $1 $2
+    echo "#####################################################" >> $2
+    printf "\n" >> $2
+    
+    let RUNCOUNTER=RUNCOUNTER+1 
+done
+
